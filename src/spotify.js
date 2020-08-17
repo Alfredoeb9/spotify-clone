57e0da9a9073fddd361b693ab0bca8b6c1b939bb
Alfredoeb9 <alfredoeb96@gmail.com>
2020-08-16 20:30:01 -0700

    created varibales for spotify

---
+++ src/spotify.js
@@ -0,0 +1,17 @@
+// https://developer.spotify.com/documentation/wep-playback-sdk/quick-start/#
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const redirectUri = 'http://localhost:3000/';
+const clientId = 'aacbe5dd08824726b4f7a7cc8b99359b';
+
+// Letting the user do these changes in the app
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state'
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
