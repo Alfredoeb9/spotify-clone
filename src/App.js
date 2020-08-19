d4c81815e0f56b5c75a5307e0cb2e442292f4468
Alfredoeb9 <alfredoeb96@gmail.com>
2020-08-18 21:54:52 -0700

    Grabbed access token from url, connected app with spotify, and fethced user data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  // Run code based on a given condition
+  useEffect(() => {
+    // grab token from url
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+      // give spotify key to react
+      spotify.setAccessToken(_token);
+      // fetch information from spotify account
+      spotify.getMe().then(user => {
+
+      });
+    }
+  }, []);
+
   return (
     <div className="app">
-
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
       <Login />
     </div>
   );
